a9c874afbdfcd95dadcee0a86fcac66a7e58a5d3
mariorodriguez1mt <mario.rodriguez1.mt@correo.usa.edu.co>
2021-12-15 10:19:36 -0500

    Se agrega mapa a la aplicacion con los marcadores de las sucursales

--- app/src/main/java/usa/app/appmovilproyectos1/FavoritosApp.java
+++ app/src/main/java/usa/app/appmovilproyectos1/FavoritosApp.java
@@ -1,10 +1,20 @@
 package usa.app.appmovilproyectos1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
@@ -17,6 +27,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import usa.app.appmovilproyectos1.databinding.ActivityFavoritosAppBinding;
 import usa.app.appmovilproyectos1.datos.DataBase;
 import usa.app.appmovilproyectos1.ui.model.Producto;
 
@@ -26,6 +37,7 @@ public class FavoritosApp extends AppCompatActivity {
     private TextView textoFav;
     private LinearLayout layoutHorizontalFav;
     private LinearLayout layoutVerticalFav;
+    private LinearLayout layoutVerticalFav2;
     private DataBase datosf;
 
 
@@ -36,6 +48,7 @@ public class FavoritosApp extends AppCompatActivity {
 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("Favoritos");
+        lyFavApp = (LinearLayout) findViewById(R.id.layfavoritosapp);
 
         ArrayList<Producto> listaFavoritos = new ArrayList<Producto>();
         datosf = new DataBase(getApplicationContext());
@@ -53,9 +66,8 @@ public class FavoritosApp extends AppCompatActivity {
                     listaFavoritos.add(producto);
 
                     }while(datosFav.moveToNext());}
-        /**
+
         for(Producto producto:listaFavoritos) {
-            if(producto.getCantidad()>0){
 
                 layoutHorizontalFav = new LinearLayout(getApplicationContext());
                 layoutHorizontalFav.setOrientation(LinearLayout.HORIZONTAL);
@@ -75,22 +87,39 @@ public class FavoritosApp extends AppCompatActivity {
 
 
                 TextView texto11 = new TextView(getApplicationContext());
-                texto11.setLayoutParams(new LinearLayout.LayoutParams(300, ViewGroup.LayoutParams.WRAP_CONTENT));
+                texto11.setLayoutParams(new LinearLayout.LayoutParams(400, ViewGroup.LayoutParams.WRAP_CONTENT));
                 texto11.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 15.f);
                 texto11.setTextColor(Color.WHITE);
                 texto11.setText(producto.getName());
 
                 TextView texto22 = new TextView(getApplicationContext());
-                texto22.setLayoutParams(new LinearLayout.LayoutParams(300, ViewGroup.LayoutParams.WRAP_CONTENT));
+                texto22.setLayoutParams(new LinearLayout.LayoutParams(400, ViewGroup.LayoutParams.WRAP_CONTENT));
                 texto22.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 15.f);
                 texto22.setTextColor(Color.WHITE);
                 texto22.setText("Descripción. " + producto.getDescription());
 
                 TextView texto33 = new TextView(getApplicationContext());
-                texto33.setLayoutParams(new LinearLayout.LayoutParams(350, ViewGroup.LayoutParams.WRAP_CONTENT));
+                texto33.setLayoutParams(new LinearLayout.LayoutParams(400, ViewGroup.LayoutParams.WRAP_CONTENT));
                 texto33.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 15.f);
                 texto33.setTextColor(Color.WHITE);
                 texto33.setText("Precio: $" + producto.getPrice());
+
+                Button button = new Button(getApplicationContext());
+                button.setLayoutParams(new LinearLayout.LayoutParams(10, ViewGroup.LayoutParams.WRAP_CONTENT,1));
+                button.setTextColor(Color.WHITE);
+                button.setText("BORRAR");
+                button.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                button.setBackgroundColor(getResources().getColor(R.color.purple_700));
+
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        datosf.deleteFavoritos(producto.getId());
+                        Toast.makeText(getApplicationContext(), "El elemento fue borrado", Toast.LENGTH_SHORT).show();
+                        finish();
+                        startActivity(getIntent());
+                    }
+                });
 
                 layoutVerticalFav.addView(texto11);
                 layoutVerticalFav.addView(texto22);
@@ -98,12 +127,12 @@ public class FavoritosApp extends AppCompatActivity {
 
                 layoutHorizontalFav.addView(image1);
                 layoutHorizontalFav.addView(layoutVerticalFav);
+                layoutHorizontalFav.addView(button);
+                layoutHorizontalFav.setGravity(Gravity.CENTER);
 
                 lyFavApp.addView(espacio1);
                 lyFavApp.addView(layoutHorizontalFav);
             }
-
-        }
 
         /**textoFav.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 18.f);
         textoFav.setTextColor(Color.WHITE);
@@ -131,15 +160,71 @@ public class FavoritosApp extends AppCompatActivity {
             }
         });**/
 
-        /**
+
         //lyFavApp.addView(precioTotal);
         TextView espacio2 = new TextView(getApplicationContext());
         espacio2.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,80));
         espacio2.setText("");
+
+        Button buttonFinal = new Button(getApplicationContext());
+        buttonFinal.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        buttonFinal.setTextColor(Color.WHITE);
+        buttonFinal.setText("BORRAR PRODUCTOS DE FAVORITOS");
+        buttonFinal.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        buttonFinal.setBackgroundColor(getResources().getColor(R.color.purple_700));
+        buttonFinal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogoDeConfirmacion ddc = new DialogoDeConfirmacion();
+                ddc.setParametros(datosf);
+                ddc.show(getSupportFragmentManager(), "DialogoDeConfirmacion");
+
+            }
+        });
+
         lyFavApp.addView(espacio2);
-        //lyFavApp.addView(buttonComp);
+        lyFavApp.addView(buttonFinal);
         //buttonComp.setGravity(Gravity.BOTTOM);**/
 
-
     }
+
+    public static class DialogoDeConfirmacion extends DialogFragment {
+
+        DataBase dataBase;
+        //SQLiteDatabase db;
+
+        public void setParametros(DataBase dataBase){
+            this.dataBase = dataBase;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the Builder class for convenient dialog construction
+            android.app.AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage("¿Está seguro de borrar todos los productos de favoritos?")
+                    .setPositiveButton("SI", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //UPDATE
+                            if(dataBase.getFavoritos().getCount()!=0) {
+                                dataBase.deleteTodo();
+                                Toast.makeText(getActivity(), "Los productos han sido borrados de favoritos", Toast.LENGTH_LONG).show();
+                                //Intent intent = new Intent(builder.getContext(),FavoritosApp.class);
+                                //startActivity(intent);
+                            }
+                            else{
+                                Toast.makeText(getActivity(), "No hay productos en favoritos", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    })
+                    .setNegativeButton("CANCELAR", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //Log.e("TAG_INFO", "Producto NO agregado a favooritos ");
+                            Toast.makeText(getActivity(), "OK", Toast.LENGTH_LONG).show();
+                        }
+                    });
+            // Create the AlertDialog object and return it
+            return builder.create();
+        }
+    }
+
 }
--- app/src/main/java/usa/app/appmovilproyectos1/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/usa/app/appmovilproyectos1/ui/slideshow/SlideshowFragment.java
@@ -1,8 +1,10 @@
 package usa.app.appmovilproyectos1.ui.slideshow;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,10 +17,12 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.CustomZoomButtonsController;
 import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -51,15 +55,63 @@ public class SlideshowFragment extends Fragment {
         binding = FragmentSlideshowBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        //basededatos = new Basededatos();
+        basededatos = new Basededatos();
+        sucursals = basededatos.getSucursals();
+
+        TextView espacio = (TextView) binding.textViewespacio;
+        espacio.setText("");
+        espacio.setTextSize(21.f);
+
+        TextView titulo = (TextView) binding.textViewMap;
+        titulo.setText("MARK HANDMADE SUCURSALES");
+        titulo.setTextSize(TypedValue.COMPLEX_UNIT_DIP,21.f);
+        titulo.setTextColor(Color.WHITE);
+        titulo.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+
+        TextView titulo2 = (TextView) binding.textViewMap2;
+        titulo2.setText("Conoce nuestros puntos de venta ubicados en la Ciudad de Bogotá D.C");
+        titulo2.setTextSize(TypedValue.COMPLEX_UNIT_DIP,15.f);
+        titulo2.setTextColor(Color.WHITE);
+        titulo2.setTextAlignment(View.TEXT_ALIGNMENT_INHERIT);
+
+
         myOpenMapView = (MapView) binding.openmapview;
+        myOpenMapView.setTileSource(TileSourceFactory.MAPNIK);
         GeoPoint bogota = new GeoPoint(4.657034, -74.105018);
+        GeoPoint puntof = new GeoPoint(0.00000,0.00000);
+        GeoPoint puntof2 = new GeoPoint(0.00000,0.00000);
+        GeoPoint puntof3 = new GeoPoint(0.00000,0.00000);
         myOpenMapView.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.ALWAYS);
+
         myMapController = (MapController) myOpenMapView.getController();
         myMapController.setCenter(bogota);
-        myMapController.setZoom(12);
-
+        myMapController.setZoom(13);
         myOpenMapView.setMultiTouchControls(true);
+        Marker punto1 = new Marker(myOpenMapView);
+        Marker punto2 = new Marker(myOpenMapView);
+        Marker punto3 = new Marker(myOpenMapView);
+
+            puntof.setCoords(sucursals.get(0).getLatitud(),sucursals.get(0).getLongitud());
+            punto1.setPosition(puntof);
+            punto1.setAnchor(Marker.ANCHOR_CENTER,Marker.ANCHOR_CENTER);
+            punto1.setTitle(sucursals.get(0).getNombre() + sucursals.get(0).getDireccion());
+            myOpenMapView.getOverlays().add(punto1);
+
+            puntof2.setCoords(sucursals.get(1).getLatitud(),sucursals.get(1).getLongitud());
+            punto2.setPosition(puntof2);
+            punto2.setAnchor(Marker.ANCHOR_CENTER,Marker.ANCHOR_CENTER);
+            punto2.setTitle(sucursals.get(1).getNombre() + sucursals.get(1).getDireccion());
+            myOpenMapView.getOverlays().add(punto2);
+
+            puntof3.setCoords(sucursals.get(2).getLatitud(),sucursals.get(2).getLongitud());
+            punto3.setPosition(puntof3);
+            punto3.setAnchor(Marker.ANCHOR_CENTER,Marker.ANCHOR_CENTER);
+            punto3.setTitle(sucursals.get(2).getNombre() + sucursals.get(2).getDireccion());
+            myOpenMapView.getOverlays().add(punto3);
+
+
+
+
         /**final TextView textView = binding.textSlideshow;
         slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
